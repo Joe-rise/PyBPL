5deb69641233886db551ab97078b66ef8124ff70
Maxwell Rebo <maxwell.b.rebo@gmail.com>
2016-01-21 23:15:14 -0500

    cpd starter; ignore IDEA files

--- bpl_base.py
+++ bpl_base.py
@@ -6,11 +6,16 @@ class BPLBase(object):
     BPL base class.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, cpd):
+        """
+        :param cpd:
+         A CPD instance (from cpd.py).
+        :return:
+        """
+        self.cpd = cpd
 
     def generate_type(self, _type):
         raise NotImplementedError("Subclass should implement generate_type")
 
-    def generate_symbol(self):
-        raise NotImplementedError("Subclass should implement generate_type")
+    def generate_token(self):
+        raise NotImplementedError("Subclass should implement generate_token")
